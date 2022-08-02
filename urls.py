43d2ae5c18f495a9ee1f13416badec46cea21160
EstebanMonsalve96 <110006079+EstebanMonsalve96@users.noreply.github.com>
2022-08-01 21:12:22 -0400

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path ('', index, name = "inicio"),
+    path ('Profesores/', profesores, name = "profesores"),
+    path ('Materias/', materias,name = "materias"),
+    path ('estudianteForm/', EstudianteFormulario, name="EstudianteFormulario") ,
+]
---
+++ views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Estudiantes, Profesores, Materias
+
+def index (request):
+    estudiantes = Estudiantes.objects.all()
+    return render (request, "inicio.html", {"Estudiante": estudiantes})
+
+def profesores (request):
+    profesores = Profesores.objects.all()
+    return render (request, "profesores.html", {"Profesor": profesores})
+
+def materias (request):
+    materias = Materias.objects.all()
+    return render (request, "materias.html", {"Materia": materias})
+
+def EstudianteFormulario(request):
+    print('method:', request.method)
+    print('post', request.POST)
+    
+    if request.method == "POST":
+        Estudiante = Estudiantes(nombre=request.POST['Nombre'], apellido=request.POST['Apellido'], email=request.POST['Email'])
+        Estudiante.save()
+        return render(request,"inicio.html")
+    return render(request, "estudianteForm.html")
+
+
